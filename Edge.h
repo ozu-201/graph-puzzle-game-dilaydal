ec5ced5d17188d7355b417979d7da821230185ff
dd028518 <dilaydal@gmail.com>
2023-12-14 14:17:06 +0300

    proje 3 lab 1

---
+++ Edge.h
@@ -0,0 +1,26 @@
+//
+// Created by dd028518 on 12/14/2023.
+//
+
+#ifndef DATASTRUCTURES_CPP_EDGE_H
+#define DATASTRUCTURES_CPP_EDGE_H
+
+
+class Edge {
+private:
+    int from;
+    int to;
+    int weight;
+    Edge* next;
+public:
+    Edge();
+    Edge(int from, int to, int weight);
+    void setNext(Edge* _next);
+    Edge* getNext() const;
+    int getFrom() const;
+    int getTo() const;
+    int getWeight() const;
+};
+
+
+#endif //DATASTRUCTURES_CPP_EDGE_H
---
+++ EdgeList.h
@@ -0,0 +1,25 @@
+//
+// Created by dd028518 on 12/14/2023.
+//
+
+#ifndef DATASTRUCTURES_CPP_EDGELIST_H
+#define DATASTRUCTURES_CPP_EDGELIST_H
+
+
+#include "Edge.h"
+
+class EdgeList {
+private:
+    Edge* head;
+    Edge* tail;
+public:
+    EdgeList();
+    ~EdgeList();
+    bool isEmpty() const;
+    Edge* search(int to) const;
+    void insert(Edge* newEdge);
+    Edge* getHead();
+};
+
+
+#endif //DATASTRUCTURES_CPP_EDGELIST_H
